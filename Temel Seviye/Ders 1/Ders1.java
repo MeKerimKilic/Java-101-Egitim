be45a82650e241e4ee43338f43f778f7e9e91499
Kerim Kılıç <51862093+MeKerimKilic@users.noreply.github.com>
2021-07-03 21:03:28 +0300

    Add files via upload

---
+++ Temel Seviye/Ders 1/Ders1.java
@@ -0,0 +1,95 @@
+
+public class Ders1 {
+
+	public static void main (String[] args) {
+	
+		
+		String stringDeger = "Merhaba ben Kerim";
+		String stringDeger1=stringDeger.replace("Merhaba","Hi");
+		stringDeger1=stringDeger1.replace("ben","I am");
+		stringDeger1=stringDeger1.replace("Kerim","Yunus");
+		System.out.println(stringDeger1);
+		
+		System.out.println("Replace()" + stringDeger.replace("Merhaba","Hi"));
+		System.out.println("Replace()" + stringDeger.replace("ben","I am"));
+		
+		System.out.println("");
+		boolean durum1=stringDeger.equals("Merhaba ben Kerim");
+		System.out.println("Durum 1 : "+ durum1);
+		
+		boolean durum2=stringDeger.equals("merhaba ben Kerim");
+		System.out.println("Durum 2 : "+ durum2);
+		
+		boolean durum3=stringDeger.equalsIgnoreCase("merhaba ben kerim");
+		System.out.println("Durum 3 : "+ durum3);
+		
+		String[] stringDegerler= stringDeger.split(" ");
+		System.out.println("1.Kelime "+ stringDegerler[0]);
+		System.out.println("2.Kelime "+ stringDegerler[1]);
+		System.out.println("3.Kelime "+ stringDegerler[2]);
+		
+		System.out.println("Kelime uzunluðu "+ stringDeger.length());
+	}
+	
+	
+	public static void mainString2 (String[] args) {
+		String stringDeger = new String("Merhaba ben Kerim");
+		char charDeger = stringDeger.charAt(9);
+		
+		System.out.println("charAt()"+charDeger);
+		
+		String substringDeger= stringDeger.substring(8);
+		
+		System.out.println("1) substring()"+substringDeger);
+		
+		String substringDeger1= stringDeger.substring(8,15);
+		
+		System.out.println("2) substring()  "+substringDeger1);
+		
+		System.out.println("toUpperCase() "+stringDeger.toUpperCase());
+		System.out.println("toLowerCase() "+stringDeger.toLowerCase());
+	}
+	
+	
+	public static void mainString1(String[] args) {
+		//char[] chars= {'M','e','r','h','a','b','a'};
+		//String stringDeger = new String(chars);
+		//bu bir konsola yazýdrma komutudur
+		//System.out.println(stringDeger);
+		
+		String stringDeger="merhaba";
+		System.out.println(stringDeger);
+	}
+	
+	
+	public static void mainVariablesType(String[] args) {
+		byte byteDeger = 4;
+		short shortDeger =7;
+		int integerDeger =234;
+		long longDeger = 12332;
+		
+		float floatDeger= 34.5f;
+		double doubleDeger = 345.2;
+		
+		char charDeger1 = 65;
+		char charDeger2 = 'A';
+		
+		boolean dogruDeger= true;
+		boolean yanlisDeger= false;
+		
+		System.out.println("Byte Deðer: "+ byteDeger);
+		System.out.println("Short Deðer: "+ shortDeger);
+		System.out.println("Integer Deðer: "+ integerDeger);
+		System.out.println("Long Deðer: "+ longDeger);
+		
+		System.out.println("Float Deðer: "+ floatDeger);
+		System.out.println("Double Deðer: "+ doubleDeger);
+		
+		System.out.println("Char Deðer 1: "+ charDeger1);
+		System.out.println("Char Deðer 2: "+ charDeger2);
+		
+		System.out.println("Boolean Doðru: "+ dogruDeger);
+		System.out.println("Boolean Yanlýþ: "+ yanlisDeger);
+	}
+
+}
